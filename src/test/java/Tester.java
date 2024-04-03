d233e34943e2c28814f0175951bdf34a366ce979
ethan-chu-shlawg <143014840+ethan-chu-shlawg@users.noreply.github.com>
2024-04-03 09:26:01 -0700

    Update Tester.java

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -19,7 +19,7 @@ public class Tester {
       int result = temp.limitAmplitude(2000);
       //answer [40, 2000, 17, -2000, -17, -2000, 2000]
       String failMsg = "Failed - Input=" + temp.samples + ", Expected 3, Output = " + result;
-      assertEquals(failMsg, 3, result);
+      assertEquals(failMsg, 3, result, 0.001);
    }
 
    @Test
@@ -29,7 +29,7 @@ public class Tester {
       int result = temp.limitAmplitude(2000);
       //answer {1048, -420, 33, 15, -32, 2000, 2000};
       String failMsg = "Failed - Input=" + temp.samples + ", Expected 2, Output = " + result;
-      assertEquals(failMsg, 2, result);
+      assertEquals(failMsg, 2, result, 0.001);
    }
 
    @Test
@@ -40,7 +40,7 @@ public class Tester {
       int result = temp.limitAmplitude(0);
       //answer [40, 2000, 17, -2000, -17, -2000, 2000]
       String failMsg = "Failed - Input=" + temp.samples + ", Expected 7, Output = " + result;
-      assertEquals(failMsg, 7, result);
+      assertEquals(failMsg, 7, result, 0.001);
    }
 
    @Test
@@ -51,7 +51,7 @@ public class Tester {
       int result = temp.limitAmplitude(1);
       //answer [40, 2000, 17, -2000, -17, -2000, 2000]
       String failMsg = "Failed - Input=" + temp.samples + ", Expected 0, Output = " + result;
-      assertEquals(failMsg, 0, result);
+      assertEquals(failMsg, 0, result, 0.001);
       //Failed - Input {} limitAmplitude(1) Expected Output 0
    }
 
@@ -63,7 +63,7 @@ public class Tester {
       int [] answer  = new int[] {2532, 17, -2300, -17, -4000, 2000};
       for (int i = 0; i < answer.length; i++){
          String failMsg = "Test failed. Expected " + answer[i] + " at sample index " + i + ", but found " + temp.samples[i];
-         assertEquals(failMsg, temp.samples[i], answer[i]);
+         assertEquals(failMsg, temp.samples[i], answer[i], 0.001);
       }
    }
 
@@ -75,7 +75,7 @@ public class Tester {
       int [] answer  = new int[] { 2532, 17, -2300, -17,0, 0, 0, -4000, 2000, 0, 0, 0};
       for (int i = 0; i < answer.length; i++){
          String failMsg = "Test failed. Expected " + answer[i] + " at sample index " + i + ", but found " + temp.samples[i];
-         assertEquals(failMsg, temp.samples[i], answer[i]);
+         assertEquals(failMsg, temp.samples[i], answer[i], 0.001);
       }
    }
 
@@ -87,7 +87,7 @@ public class Tester {
       int [] answer  = new int[] {1};
       for (int i = 0; i < answer.length; i++){
          String failMsg = "Test failed. Expected " + answer[i] + " at sample index " + i + ", but found " + temp.samples[i];
-         assertEquals(failMsg, temp.samples[i], answer[i]);
+         assertEquals(failMsg, temp.samples[i], answer[i], 0.001);
     }
    //Failed - Input {1} Expected Output <nothing> but samples should be {1}
 }
